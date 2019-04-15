f3e9e7b584656e6ee15645141c419829a9985ea8
Saúl Enrique Melchor Ramírez <saulenriquemr@outlook.com>
2019-04-15 14:13:35 -0500

    Problema 3 resuelto en Java

---
+++ 3-Stripe/Stripe.java
@@ -0,0 +1,48 @@
+/**
+ * Stripe
+ */
+
+
+ /**
+  * This problem was asked by Stripe.
+  *
+  * Given an array of integers, find the first missing 
+  * positive integer in linear time and constant space. 
+  * In other words, find the lowest positive integer that 
+  * does not exist in the array. The array can contain 
+  * duplicates and negative numbers as well.
+  *
+  * For example, the input [3, 4, -1, 1] should give 2. 
+  * The input [1, 2, 0] should give 3.
+  * You can modify the input array in-place
+  */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Stripe {
+
+    public int obtenerProximoEntero() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Número de elementos: ");
+        int numeroDeElementos = Integer.parseInt(teclado.nextLine());
+        ArrayList<Integer> listaDeElementos = new ArrayList<>();
+
+        for (int i = 0; i < numeroDeElementos; i++) {
+            int elemento = Integer.parseInt(teclado.nextLine());
+            listaDeElementos.add(elemento);
+        }
+
+        Collections.sort(listaDeElementos);
+
+        return (listaDeElementos.get(listaDeElementos.size() - 1)) + 1;
+
+    }
+
+    public static void main(String[] args) {
+        Stripe s = new Stripe();
+        System.out.println(s.obtenerProximoEntero());
+    }
+    
+}
