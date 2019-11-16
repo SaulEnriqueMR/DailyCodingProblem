cc6c8d4f16f733f8071b12f95b410dd7a01ff34b
Saúl Enrique Melchor Ramírez <SaulEnriqueMR@outlook.com>
2019-11-16 09:28:06 -0600

    Problema MinMaxSum de HackerRank: -Resuelto en .Net Core

---
+++ MinMaxSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace MiniMaxSum
+{
+    class Program
+    {
+        static void miniMaxSum(int[] arr) {
+            Array.Sort(arr);
+            long minimo = 0;
+            long maximo = 0;
+            for (int i = 0; i <= 3; i++)
+            {
+                minimo += arr[i];    
+            }
+            for (int i = 1; i <= 4; i++)
+            {
+                maximo += arr[i];
+            }
+            Console.WriteLine(String.Format("{0} {1}", minimo, maximo));
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+        }
+    }
+}
