3d7569ddaf4e47c2bb5b8a90f17cd0c01c2e82b7
Saúl Enrique Melchor Ramírez <SaulEnriqueMR@outlook.com>
2019-11-18 21:08:19 -0600

    Ejercicio Staircases de HackerRank -Resuelto en C#

---
+++ Staircase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Staircase(int n) {
+            StringBuilder resultado = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                int diferencia = n-i;
+                for (int j = 0; j < diferencia-1; j++)
+                {
+                    resultado.Append(" ");
+                }
+
+                for (int j = diferencia; j <= n; j++)
+                {
+                    resultado.Append("#");
+                }
+
+                if (diferencia > 1)
+                {
+                    resultado.Append("\n");
+                }
+                
+            }
+            Console.Write(resultado.ToString());
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Staircase(n);
+        }
+    }
+}
